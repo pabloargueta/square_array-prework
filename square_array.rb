2972e1993111b8d3def83b13f66f4c00b19b672a
Pablo Argueta <pabloargueta27@gmail.com>
2018-06-16 02:04:37 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,3 @@
 def square_array(array)
-  #array.collect {|number| number ** 2}  
-  
-  new_array = []
-  
-  array.each do |number|
-    new_array.push(number**2)
-  end
-  
-  new_array
-  
+  array.collect {|number| number ** 2}
 end
